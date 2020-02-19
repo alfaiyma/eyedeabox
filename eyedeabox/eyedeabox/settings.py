f16ecc9480e20914ca6639a2b7b19246117561ae
mohammad al faiyaz <mohammad@mohammad-alfaiyaz.com>
2020-02-18 22:19:16 -0500

    Update nav bar for responsive-ness & allow local host

--- eyedeabox/eyedeabox/settings.py
+++ eyedeabox/eyedeabox/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 's1$7!gw&*1)ir@=qodz&8o%wl%4rt&&fo7qf#_z5^$0pnv#w5s'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['malfaiyaz.pythonanywhere.com']
+ALLOWED_HOSTS = ['127.0.0.1', 'malfaiyaz.pythonanywhere.com']
 
 
 # Application definition
